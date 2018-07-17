985b340d974c5673a8042bef175d7a4893cada30
恩和 <en.he@9douyu.com>
2018-07-18 00:13:15 +0800

    docker shell

---
+++ shell/docker.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -u
+
+function help()
+{
+    cat <<EOF
+
+
+Used : sh $0 COMMAND [OPTIONS]
+
+
+OPTIONS :
+
+    --name      named a container with --name
+    
+    --port      expose a port to local machine
+
+    --help      show the more information with this command tools
+
+
+EOF
+
+}
+
+
+# Run 
+
+$@
